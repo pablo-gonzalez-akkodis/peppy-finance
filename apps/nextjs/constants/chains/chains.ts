00417c749c804e6656f7516edd39e3343c0bd6cc
Alit dev <22934057+alitdev@users.noreply.github.com>
2023-10-26 23:47:55 +0330

    #19 move app_chain to client

--- apps/nextjs/constants/chains/chains.ts
+++ apps/nextjs/constants/chains/chains.ts
@@ -1,3 +1,19 @@
 import { SupportedChainId } from "@symmio-client/core/constants/chains";
+import { bsc, fantom, base, polygon, arbitrum, mainnet } from "wagmi/chains";
+
+const supportedWagmiChain = {
+  [SupportedChainId.FANTOM]: fantom,
+  [SupportedChainId.BSC]: bsc,
+  [SupportedChainId.BASE]: base,
+  [SupportedChainId.POLYGON]: polygon,
+  [SupportedChainId.ARBITRUM]: arbitrum,
+  [SupportedChainId.MAINNET]: mainnet,
+};
+
+function getWagmiChain(supportChainList: number[]) {
+  return supportChainList.map((chainId) => supportedWagmiChain[chainId]);
+}
 
 export const ClientChain = [SupportedChainId.FANTOM, SupportedChainId.BSC];
+
+export const APP_CHAINS = getWagmiChain(ClientChain);
--- packages/core/src/constants/chains.ts
+++ packages/core/src/constants/chains.ts
@@ -1,10 +1,7 @@
-import { bsc, fantom } from "wagmi/chains";
-
 export enum SupportedChainId {
   MAINNET = 1,
   ROPSTEN = 3,
   RINKEBY = 4,
-
   BSC = 56,
   BASE = 8453,
   BSC_TESTNET = 97,
@@ -22,8 +19,6 @@ export function isSupportedChain(
 ): chainId is SupportedChainId {
   return !!chainId && !!SupportedChainId[chainId];
 }
-
-export const APP_CHAINS = [fantom, bsc];
 
 export const FALLBACK_CHAIN_ID = SupportedChainId.BSC;
 
