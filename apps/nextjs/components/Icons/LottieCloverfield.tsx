47c7ef8e85b43b6d0f966b805bb9e28a6d6725dd
Mohammad Ehsan Kaji <mr.ehsan.kaji@gmail.com>
2024-05-26 16:32:25 +0330

    Fix build error

--- apps/nextjs/components/Icons/LottieCloverfield.tsx
+++ apps/nextjs/components/Icons/LottieCloverfield.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Lottie from "react-lottie";
 import * as animationData from "constants/lottie/cloverfield-loading.json";
 
